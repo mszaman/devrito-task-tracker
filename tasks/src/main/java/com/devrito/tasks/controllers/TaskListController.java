e5045a5c5089289e96569ad91bc4fd6c93672aa6
mszaman <zaman.msz@outlook.com>
2025-11-11 15:02:40 +0600

    add delete task list functionality.

--- tasks/src/main/java/com/devrito/tasks/controllers/TaskListController.java
+++ tasks/src/main/java/com/devrito/tasks/controllers/TaskListController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,12 @@ public class TaskListController {
 		TaskList updatedTaskList = taskListService.updateTaskList(taskListId, taskListMapper.fromDto(taskListDto));
 
 		return taskListMapper.toDto(updatedTaskList);
+	}
+
+	@DeleteMapping(path = "/{task_list_id}")
+	public void deleteTaskList(@PathVariable("task_list_id") UUID taskListId) {
+
+		taskListService.deleteTaskList(taskListId);
+		;
 	}
 }
